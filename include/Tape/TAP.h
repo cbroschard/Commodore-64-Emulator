f8f8768159e2cc0b84f82dc8915ea531a40f2049
Christopher Broschard <cbroschard@hotmail.com>
2025-10-01 16:04:01 -0400

    Fixed gitignore to include Tape includes!

---
+++ include/Tape/TAP.h
@@ -0,0 +1,79 @@
+﻿// Copyright (c) 2025 Christopher Broschard
+// All rights reserved.
+//
+// This source code is provided for personal, educational, and
+// non-commercial use only. Redistribution, modification, or use
+// of this code in whole or in part for any other purpose is
+// strictly prohibited without the prior written consent of the author.
+#ifndef TAP_H
+#define TAP_H
+
+#include "TapeImage.h"
+
+class TAP : public TapeImage
+{
+    public:
+        TAP();
+        virtual ~TAP();
+        bool loadTape(const std::string& filePath, VideoMode mode) override;
+        void rewind() override;
+        void simulateLoading() override;
+        bool currentBit() const override;
+
+        void attachLoggingInstance(Logging* logger);
+
+        // Monitor helpers
+        inline uint8_t debugTapeVersion() const override { return header.tapeVersion; }
+        inline size_t debugPulseIndex() const override { return pulseIndex; }
+        inline size_t debugPulseCount() const override { return pulses.size(); }
+        inline uint32_t debugPulseRemaining() const override { return pulseRemaining; }
+        uint32_t debugCurrentPulse() const;
+        uint32_t debugNextPulse(size_t lookahead = 1) const override;
+
+
+
+    protected:
+        std::vector<uint8_t> tapeData; // vector to store the tape file
+
+    private:
+
+        // Non-owning pointers
+        Logging* logger = nullptr;
+
+        static constexpr double PAL_CLOCK = 985248.0;
+        static constexpr double NTSC_CLOCK = 1022727.0;
+
+        #pragma pack(push,1)
+        struct tapeHeader
+        {
+            char fileSignature[12]; // Should equate to either C64-TAPE-RAW or C16-TAPE-RAW depending on version
+            uint8_t tapeVersion;
+            uint8_t platform;       // 0 = C64, 1 = VIC-20, 2 = C16, Plus/4, 3 = PET, 4 = C5x0, 5 = C6x0, C7x0
+            uint8_t videoStandard;  // 0 = PAL, 1 = NTSC, 2 = OLD NTSC, 3 = PALN
+            uint8_t reserved;
+            uint32_t dataSize;      // File data size without tape header in LOW/HIGH format
+        } header;
+        #pragma pack(pop)
+
+        // Process pulses
+        struct tapePulse
+        {
+            uint32_t duration;
+            bool isGap; // true = "silence" , false = "active pulse"
+        };
+
+        std::vector<tapePulse> parsePulses(VideoMode mode);
+        std::vector<tapePulse> pulses;
+        size_t pulseIndex;
+        uint32_t pulseRemaining;
+        bool currentLevel;
+
+        // Loading and validation
+        virtual bool loadFile(const std::string& path, std::vector<uint8_t>& buffer) override;
+        virtual bool validateHeader() override;
+
+        // Helpers to determine proper video mode and scaling
+        uint64_t determineScaleCycles(uint64_t tapeCycles, bool tapeIsNTSC, bool emuIsNTSC);
+};
+
+#endif // TAP_H
---
+++ include/Tape/TapeImage.h
@@ -0,0 +1,46 @@
+﻿// Copyright (c) 2025 Christopher Broschard
+// All rights reserved.
+//
+// This source code is provided for personal, educational, and
+// non-commercial use only. Redistribution, modification, or use
+// of this code in whole or in part for any other purpose is
+// strictly prohibited without the prior written consent of the author.
+#ifndef TAPEIMAGE_H
+#define TAPEIMAGE_H
+
+#include <cstring>
+#include <cstdint>
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <vector>
+#include "common.h"
+#include "Logging.h"
+
+class TapeImage
+{
+    public:
+        TapeImage();
+        virtual ~TapeImage();
+
+        virtual bool loadTape(const std::string& filePath, VideoMode mode) = 0;
+        virtual void rewind() = 0;
+        virtual void simulateLoading() = 0;
+        virtual bool currentBit() const = 0;
+        virtual bool isT64() const;
+
+        // Debug API (default no-op)
+        virtual uint8_t debugTapeVersion() const { return 0; }
+        virtual size_t debugPulseIndex() const { return 0; }
+        virtual size_t debugPulseCount() const { return 0; }
+        virtual uint32_t debugNextPulse(size_t offset = 0) const { return 0; }
+        virtual uint32_t debugPulseRemaining() const { return 0; }
+
+    protected:
+
+    private:
+        virtual bool loadFile(const std::string& path, std::vector<uint8_t>& buffer) = 0;
+        virtual bool validateHeader() = 0;
+};
+
+#endif // TAPEIMAGE_H
