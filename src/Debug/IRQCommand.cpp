b7ff5b4b9aa18da0afaf52cb89850f94283838ed
Christopher Broschard <cbroschard@hotmail.com>
2025-10-11 13:32:18 -0400

    Fixed help and shorthep

--- src/Debug/IRQCommand.cpp
+++ src/Debug/IRQCommand.cpp
@@ -30,7 +30,7 @@ std::string IRQCommand::name() const
 
 std::string IRQCommand::shortHelp() const
 {
-    return "irq [off|on|clear|vic <mask>|cia1 <mask>|cia2 <mask>]";
+    return "irq [status|off|on|restore|clear|vic <mask>|cia1 <mask>|cia2 <mask>]";
 }
 
 std::string IRQCommand::help() const
@@ -38,10 +38,12 @@ std::string IRQCommand::help() const
     return
           "irq off        - Disable all IRQ sources (VIC + CIA1 + CIA2) and clear any pending.\n"
           "irq on         - Restore IRQ enables from snapshot (taken when 'irq off' ran).\n"
+          "irq status     - Displays current status of all IRQs\n"
           "irq clear      - Acknowledge/clear any pending interrupts without changing masks.\n"
-          "irq vic <m>    - Set VIC $D01A to mask m (hex or dec). Bits: 0=raster,1=spr-spr,2=spr-bg,3=lightpen.\n"
+          "irq restore    - Restores the original configuration\n"
+          "irq vic <m>    - Set VIC $D01A to mask m (hex or dec). Bits: 0=raster,1=spr-bg,2=spr-spr,3=lightpen.\n"
           "irq cia1 <m>   - Enable CIA1 IER bits m (0..31). (Write-only on HW; monitor remembers what it sets.)\n"
-          "irq cia2 <m>   - Enable CIA2 IER bits m (0..31).\n";
+          "irq cia2 <m>   - Enable CIA2 IER bits m (0..31). (CIA2 drives NMI.)\n";
 }
 
 void IRQCommand::execute(MLMonitor& mon, const std::vector<std::string>& args)
