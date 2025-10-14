f7ff40dfbfe3fb329c5ff5cc26889ced8fc11e7a
Christopher Broschard <cbroschard@hotmail.com>
2025-10-13 20:46:41 -0400

    Updated to use same register dump as r

--- src/Debug/StepCommand.cpp
+++ src/Debug/StepCommand.cpp
@@ -73,11 +73,37 @@ void StepCommand::execute(MLMonitor& mon, const std::vector<std::string>& args)
 
     // Dump CPU registers
     auto st = mon.computer()->getCPUState();
-    std::cout << "PC=$" << std::setw(4) << std::setfill('0') << std::hex << std::uppercase << st.PC
-        << "  A=$" << std::setw(2) << int(st.A)
-        << "  X=$" << std::setw(2) << int(st.X)
-        << "  Y=$" << std::setw(2) << int(st.Y)
-        << "  SP=$" << std::setw(2) << int(st.SP)
-        << "  P=$" << std::setw(2) << int(st.SR)
-        << "  (NV-BDIZC)\n";
+    auto hex2 = [](uint32_t v){
+        std::ostringstream s;
+        s << std::uppercase << std::hex << std::setw(2) << std::setfill('0') << (v & 0xFF);
+        return s.str();
+    };
+    auto hex4 = [](uint32_t v){
+        std::ostringstream s;
+        s << std::uppercase << std::hex << std::setw(4) << std::setfill('0') << (v & 0xFFFF);
+        return s.str();
+    };
+    auto flagsBits = [&](uint8_t p){
+        std::string b;
+        b += (p & 0x80) ? '1' : '0'; // N
+        b += (p & 0x40) ? '1' : '0'; // V
+        b += '-';                    // (unused)
+        b += (p & 0x10) ? '1' : '0'; // B
+        b += (p & 0x08) ? '1' : '0'; // D
+        b += (p & 0x04) ? '1' : '0'; // I
+        b += (p & 0x02) ? '1' : '0'; // Z
+        b += (p & 0x01) ? '1' : '0'; // C
+        return b;
+    };
+
+    std::ostringstream out;
+    out << "PC=$" << hex4(st.PC)
+         << "  A=$" << hex2(st.A)
+         << "  X=$" << hex2(st.X)
+         << "  Y=$" << hex2(st.Y)
+         << "  SP=$" << hex2(st.SP)
+         << "  P=$" << hex2(st.SR)
+         << "  (NV-BDIZC=" << flagsBits(st.SR) << ")\n";
+
+    std::cout << out.str();
 }
