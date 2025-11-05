b67fcfb06d5f54f5a3c8c0bc1a88ea02d33ee288
Christopher Broschard <cbroschard@hotmail.com>
2025-11-04 21:08:28 -0500

    Initial 1571

---
+++ include/Drive/D1571Memory.h
@@ -0,0 +1,23 @@
+// Copyright (c) 2025 Christopher Broschard
+// All rights reserved.
+//
+// This source code is provided for personal, educational, and
+// non-commercial use only. Redistribution, modification, or use
+// of this code in whole or in part for any other purpose is
+// strictly prohibited without the prior written consent of the author.
+#ifndef D1571MEMORY_H
+#define D1571MEMORY_H
+
+
+class D1571Memory
+{
+    public:
+        D1571Memory();
+        virtual ~D1571Memory();
+
+    protected:
+
+    private:
+};
+
+#endif // D1571MEMORY_H
---
+++ src/Drive/D1571Memory.cpp
@@ -0,0 +1,18 @@
+// Copyright (c) 2025 Christopher Broschard
+// All rights reserved.
+//
+// This source code is provided for personal, educational, and
+// non-commercial use only. Redistribution, modification, or use
+// of this code in whole or in part for any other purpose is
+// strictly prohibited without the prior written consent of the author.
+#include "D1571Memory.h"
+
+D1571Memory::D1571Memory()
+{
+    //ctor
+}
+
+D1571Memory::~D1571Memory()
+{
+    //dtor
+}
