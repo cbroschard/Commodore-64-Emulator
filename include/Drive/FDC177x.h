6cd0b4e75a9e5d9bc974702fc7cd32afb4a236c5
Christopher Broschard <cbroschard@hotmail.com>
2025-11-04 22:11:46 -0500

    Initial FDC Controller commit

---
+++ include/Drive/FDC177x.h
@@ -0,0 +1,23 @@
+// Copyright (c) 2025 Christopher Broschard
+// All rights reserved.
+//
+// This source code is provided for personal, educational, and
+// non-commercial use only. Redistribution, modification, or use
+// of this code in whole or in part for any other purpose is
+// strictly prohibited without the prior written consent of the author.
+#ifndef FDC177X_H
+#define FDC177X_H
+
+
+class FDC177x
+{
+    public:
+        FDC177x();
+        virtual ~FDC177x();
+
+    protected:
+
+    private:
+};
+
+#endif // FDC177X_H
---
+++ src/Drive/FDC177x.cpp
@@ -0,0 +1,18 @@
+// Copyright (c) 2025 Christopher Broschard
+// All rights reserved.
+//
+// This source code is provided for personal, educational, and
+// non-commercial use only. Redistribution, modification, or use
+// of this code in whole or in part for any other purpose is
+// strictly prohibited without the prior written consent of the author.
+#include "FDC177x.h"
+
+FDC177x::FDC177x()
+{
+    //ctor
+}
+
+FDC177x::~FDC177x()
+{
+    //dtor
+}
