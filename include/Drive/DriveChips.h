652ebc250e74f821178303b35c54a429378c9f7f
Christopher Broschard <cbroschard@hotmail.com>
2025-11-23 19:32:23 -0500

    Added this to allow deriving for mlmonitor through Drive class for model specific drives

---
+++ include/Drive/DriveChips.h
@@ -0,0 +1,19 @@
+#ifndef DRIVECHIPS_H_INCLUDED
+#define DRIVECHIPS_H_INCLUDED
+
+class DriveMemoryBase {
+public:
+    virtual ~DriveMemoryBase() = default;
+};
+
+class DriveCIABase {
+public:
+    virtual ~DriveCIABase() = default;
+};
+
+class DriveVIABase {
+public:
+    virtual ~DriveVIABase() = default;
+};
+
+#endif // DRIVECHIPS_H_INCLUDED
