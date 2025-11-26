28d453a22492a36bdc5343f396b86b66a8a250a5
Christopher Broschard <cbroschard@hotmail.com>
2025-11-25 21:45:40 -0500

    Added support for CIA in ML Monitor

--- include/Drive/DriveChips.h
+++ include/Drive/DriveChips.h
@@ -11,6 +11,33 @@ public:
 class DriveCIABase {
 public:
     virtual ~DriveCIABase() = default;
+
+        // ML Monitor
+        struct ciaRegsView
+        {
+            const uint8_t& portA;
+            const uint8_t& portB;
+            const uint8_t& ddrA;
+            const uint8_t& ddrB;
+            const uint8_t& tAL;
+            const uint8_t& tAH;
+            const uint8_t& tBL;
+            const uint8_t& tBH;
+            const uint8_t& tod10;
+            const uint8_t& todSec;
+            const uint8_t& todMin;
+            const uint8_t& todHour;
+            const uint8_t& sd;
+            const uint8_t& ier;
+            const uint8_t& cra;
+            const uint8_t& crb;
+            const uint16_t& tA;
+            const uint16_t& taLAT;
+            const uint16_t& tB;
+            const uint16_t& tbLAT;
+        };
+
+        virtual ciaRegsView getRegsView() const = 0;
 };
 
 class DriveVIABase {
