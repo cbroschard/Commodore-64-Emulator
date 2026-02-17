74de594a764b13c24353228991d1bca4b017ce28
Christopher Broschard <cbroschard@hotmail.com>
2026-02-16 22:13:47 -0500

    Centralized DriveModel enum

--- include/common.h
+++ include/common.h
@@ -20,6 +20,27 @@
 // Cartridge memory location
 enum cartLocation { LO, HI };
 
+// Drive models
+enum class DriveModel : uint8_t
+{
+    None  = 0,
+    D1541 = 1,
+    D1571 = 2,
+    D1581 = 3
+};
+
+inline const char* driveModelToString(DriveModel m)
+{
+    switch (m)
+    {
+        case DriveModel::None:  return "none";
+        case DriveModel::D1541: return "1541";
+        case DriveModel::D1571: return "1571";
+        case DriveModel::D1581: return "1581";
+        default: return "None";
+    }
+}
+
 // Video mode
 enum class VideoMode { NTSC, PAL};
 
