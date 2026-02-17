b4e3f1a6370f98b6d1b2fed03bd69cc33eb0e681
Christopher Broschard <cbroschard@hotmail.com>
2026-02-17 09:18:31 -0500

    Added missing license

--- include/Common/DriveTypes.h
+++ include/Common/DriveTypes.h
@@ -1,3 +1,10 @@
+// Copyright (c) 2025 Christopher Broschard
+// All rights reserved.
+//
+// This source code is provided for personal, educational, and
+// non-commercial use only. Redistribution, modification, or use
+// of this code in whole or in part for any other purpose is
+// strictly prohibited without the prior written consent of the author.
 #ifndef DRIVETYPES_H_INCLUDED
 #define DRIVETYPES_H_INCLUDED
 
