317b2ef0f870742111fc6c311433b48b6284cfed
Christopher Broschard <cbroschard@hotmail.com>
2026-05-08 07:47:13 -0400

    vic: remove unused memory base helper accessors

--- include/Vic.h
+++ include/Vic.h
@@ -88,10 +88,6 @@ class Vic
         inline bool getLatchedCSEL(int raster) const { return (latchedD016ForRaster(raster) & 0x08) != 0; }
 
         // Getters for current memory locations
-        inline uint16_t getCHARBase(int raster) const { return ((effectiveD018ForRaster(raster) >> 1) & 0x07) * 0x0800; }
-        inline uint16_t getScreenBase(int raster) const { return ((effectiveD018ForRaster(raster) & 0xF0) << 6); }
-        inline uint16_t getBitmapBase(int raster) const { return ((effectiveD018ForRaster(raster) >> 3) & 0x01) * 0x2000; }
-        inline uint16_t getLatchedCHARBase(int raster) const { return ((latchedD018ForRaster(raster) >> 1) & 0x07) * 0x0800; }
         inline uint16_t getLatchedScreenBase(int raster) const { return ((latchedD018ForRaster(raster) & 0xF0) << 6); }
         inline uint16_t getLatchedBitmapBase(int raster) const { return ((latchedD018ForRaster(raster) >> 3) & 0x01) * 0x2000; }
 
