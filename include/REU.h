36445e2a37671e839efc8c1d0efbb3365f016ba3
Christopher Broschard <cbroschard@hotmail.com>
2026-05-11 13:27:23 -0400

    reu: Added skeleton class

---
+++ include/REU.h
@@ -0,0 +1,23 @@
+// Copyright (c) 2025 Christopher Broschard
+// All rights reserved.
+//
+// This source code is provided for personal, educational, and
+// non-commercial use only. Redistribution, modification, or use
+// of this code in whole or in part for any other purpose is
+// strictly prohibited without the prior written consent of the author.
+#ifndef REU_H
+#define REU_H
+
+
+class REU
+{
+    public:
+        REU();
+        virtual ~REU();
+
+    protected:
+
+    private:
+};
+
+#endif // REU_H
