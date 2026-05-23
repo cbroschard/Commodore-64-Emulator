89f880de9f93f1395332437f4f24eaba7f4e49b8
Christopher Broschard <cbroschard@hotmail.com>
2026-05-22 22:17:37 -0400

    driveVIA6522: new skeleton via6522 class for 1541/1571

---
+++ src/Drive/driveVIA6522.cpp
@@ -0,0 +1,18 @@
+// Copyright (c) 2025 Christopher Broschard
+// All rights reserved.
+//
+// This source code is provided for personal, educational, and
+// non-commercial use only. Redistribution, modification, or use
+// of this code in whole or in part for any other purpose is
+// strictly prohibited without the prior written consent of the author.
+#include "driveVIA6522.h"
+
+driveVIA6522::driveVIA6522()
+{
+    //ctor
+}
+
+driveVIA6522::~driveVIA6522()
+{
+    //dtor
+}
