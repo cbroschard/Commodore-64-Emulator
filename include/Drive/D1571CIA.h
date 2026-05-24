2c80b5aa56239e802529606b8b27f6b78421c5c4
Christopher Broschard <cbroschard@hotmail.com>
2026-05-23 20:55:08 -0400

    d1571 d1581: new derived cia class skeletons

---
+++ include/Drive/D1571CIA.h
@@ -0,0 +1,24 @@
+// Copyright (c) 2025 Christopher Broschard
+// All rights reserved.
+//
+// This source code is provided for personal, educational, and
+// non-commercial use only. Redistribution, modification, or use
+// of this code in whole or in part for any other purpose is
+// strictly prohibited without the prior written consent of the author.
+#ifndef D1571CIA_H
+#define D1571CIA_H
+
+#include "Drive/DriveCIA.h"
+
+class D1571CIA : public DriveCIA
+{
+    public:
+        D1571CIA();
+        ~D1571CIA() override;
+
+    protected:
+
+    private:
+};
+
+#endif // D1571CIA_H
---
+++ include/Drive/D1581CIA.h
@@ -0,0 +1,24 @@
+// Copyright (c) 2025 Christopher Broschard
+// All rights reserved.
+//
+// This source code is provided for personal, educational, and
+// non-commercial use only. Redistribution, modification, or use
+// of this code in whole or in part for any other purpose is
+// strictly prohibited without the prior written consent of the author.
+#ifndef D1581CIA_H
+#define D1581CIA_H
+
+#include "Drive/DriveCIA.h"
+
+class D1581CIA : public DriveCIA
+{
+    public:
+        D1581CIA();
+        ~D1581CIA() override;
+
+    protected:
+
+    private:
+};
+
+#endif // D1581CIA_H
