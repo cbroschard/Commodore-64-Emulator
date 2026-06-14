f5ea9c8fb13bf7a160216f01de5b5f3b806ccfe1
Christopher Broschard <cbroschard@hotmail.com>
2026-06-13 20:45:34 -0400

    TraceCommand: Added missing header post CIA2 cleanup

--- src/Debug/TraceCommand.cpp
+++ src/Debug/TraceCommand.cpp
@@ -5,6 +5,7 @@
 // non-commercial use only. Redistribution, modification, or use
 // of this code in whole or in part for any other purpose is
 // strictly prohibited without the prior written consent of the author.
+#include "Common/HexFormat.h"
 #include "Debug/MLMonitor.h"
 #include "Debug/TraceCommand.h"
 
