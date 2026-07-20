9ba239df7f0bad8ec4c3cab8f07bd0e95fd409dd
Christopher Broschard <cbroschard@hotmail.com>
2026-07-20 17:34:45 -0400

    DebugManager: Remove logging

--- include/DebugManager.h
+++ include/DebugManager.h
@@ -27,7 +27,6 @@ class IECBUS;
 class IO;
 class IRQLine;
 class Keyboard;
-class Logging;
 class Memory;
 class PLA;
 class REU;
@@ -60,7 +59,6 @@ class DebugManager
                          IO* io,
                          IRQLine* irq,
                          Keyboard* keyb,
-                         Logging* log,
                          Memory* mem,
                          PLA* pla,
                          REU* reu,
