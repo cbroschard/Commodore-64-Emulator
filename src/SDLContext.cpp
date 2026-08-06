5991a7560cd2d1be5bdf1a6ab0b35ad3c56c52d6
Christopher Broschard <cbroschard@hotmail.com>
2026-08-06 19:58:06 -0400

    SDLContext: Conversion from SDL2 to SDL3

--- src/SDLContext.cpp
+++ src/SDLContext.cpp
@@ -5,27 +5,26 @@
 // non-commercial use only. Redistribution, modification, or use
 // of this code in whole or in part for any other purpose is
 // strictly prohibited without the prior written consent of the author.
-#include <SDL2/SDL.h>
+#include <SDL3/sdl.h>
 #include <stdexcept>
 #include <string>
 #include "SDLContext.h"
 
 SDLContext::SDLContext()
 {
-    const Uint32 flags =
+    const SDL_InitFlags flags =
         SDL_INIT_VIDEO |
         SDL_INIT_AUDIO |
-        SDL_INIT_GAMECONTROLLER |
-        SDL_INIT_JOYSTICK |
+        SDL_INIT_GAMEPAD |
         SDL_INIT_HAPTIC;
 
-     if (SDL_Init(flags) != 0)
-        {
-            throw std::runtime_error(
-                std::string("SDL initialization failed: ") +
-                SDL_GetError()
-            );
-        }
+    if (!SDL_Init(flags))
+    {
+        throw std::runtime_error(
+            std::string("SDL initialization failed: ") +
+            SDL_GetError()
+        );
+    }
 }
 
 SDLContext::~SDLContext()
