53f784b996831071772d6a444be24b282ef31961
Christopher Broschard <cbroschard@hotmail.com>
2026-08-06 18:47:07 -0400

    AudioOutput: Convert from SDL2 to SDL3

--- include/AudioOutput.h
+++ include/AudioOutput.h
@@ -8,7 +8,7 @@
 #ifndef AUDIOOUTPUT_H
 #define AUDIOOUTPUT_H
 
-#include "SDL2/SDL.h"
+#include <SDL3/SDL.h>
 #include "SID/sid.h"
 
 class AudioOutput
@@ -29,23 +29,22 @@ class AudioOutput
         void pauseAudio();
         void stopAudio();
         void resumeAudio();
-        inline int getBlockSamples() const { return obtainedSpec.samples; }
-        inline int getSampleRate() const { return obtainedSpec.freq; }
-        void fillAudioBuffer(Uint8* stream, int len);
+        inline int getBlockSamples() const { return BUFFER_SIZE; }
+        inline int getSampleRate() const { return SAMPLE_RATE; }
+        void fillAudioBuffer(Uint8* buffer, int len);
 
     protected:
 
     private:
-        // Non-owning pointers
+        // Non-owning pointer
         SID* sid;
 
-        static const int SAMPLE_RATE = 44100;
-        static const int CHANNELS = 2;
-        static const int BUFFER_SIZE = 2048;
+        // SDL-owned audio stream handle
+        SDL_AudioStream* stream;
 
-        SDL_AudioSpec desired{};
-        SDL_AudioSpec obtainedSpec{};
-        SDL_AudioDeviceID dev;
+        static constexpr int SAMPLE_RATE = 44100;
+        static constexpr int CHANNELS = 2;
+        static constexpr int BUFFER_SIZE = 2048;
 };
 
 #endif // AUDIOOUTPUT_H
--- src/AudioOutput.cpp
+++ src/AudioOutput.cpp
@@ -5,21 +5,39 @@
 // non-commercial use only. Redistribution, modification, or use
 // of this code in whole or in part for any other purpose is
 // strictly prohibited without the prior written consent of the author.
+#include <vector>
 #include "AudioOutput.h"
 
 namespace
 {
-    void audioCallback(void* userdata, Uint8* stream, int len)
+    void SDLCALL audioCallback(void* userdata, SDL_AudioStream* stream, int additionalAmount, int totalAmount)
     {
-        AudioOutput* audioOutput = static_cast<AudioOutput*>(userdata);
-        audioOutput->fillAudioBuffer(stream, len);
+        (void)totalAmount;
+
+        auto* audioOutput = static_cast<AudioOutput*>(userdata);
+
+        if (!audioOutput || !stream || additionalAmount <= 0)
+            return;
+
+        const int bytesPerFrame =  static_cast<int>(sizeof(Sint16)) * 2;
+        const int frameCount = additionalAmount / bytesPerFrame;
+
+        if (frameCount <= 0)
+            return;
+
+        std::vector<Sint16> buffer(frameCount * 2);
+        const int bytesToGenerate = frameCount * bytesPerFrame;
+        audioOutput->fillAudioBuffer(reinterpret_cast<Uint8*>(buffer.data()), bytesToGenerate);
+
+        if (!SDL_PutAudioStreamData(stream, buffer.data(), bytesToGenerate))
+            SDL_LogError(SDL_LOG_CATEGORY_AUDIO, "Couldn't provide audio data: %s", SDL_GetError());
     }
 }
 
 
 AudioOutput::AudioOutput() :
     sid(nullptr),
-    dev(0)
+    stream(nullptr)
 {
 
 }
@@ -29,10 +47,13 @@ AudioOutput::~AudioOutput()
     stopAudio();
 }
 
-void AudioOutput::fillAudioBuffer(Uint8* stream, int len)
+void AudioOutput::fillAudioBuffer(Uint8* outputBuffer, int len)
 {
-    Sint16* buffer = reinterpret_cast<Sint16*>(stream);
-    int numSamplesPerChannel = len / (sizeof(Sint16) * CHANNELS);
+    if (!outputBuffer || len <= 0)
+        return;
+
+    Sint16* buffer = reinterpret_cast<Sint16*>(outputBuffer);
+    const int numSamplesPerChannel = len / (sizeof(Sint16) * CHANNELS);
 
     for (int i = 0; i < numSamplesPerChannel; ++i)
     {
@@ -52,39 +73,41 @@ void AudioOutput::fillAudioBuffer(Uint8* stream, int len)
 
 bool AudioOutput::playAudio()
 {
-    desired.freq = SAMPLE_RATE;
-    desired.format = AUDIO_S16SYS;
-    desired.channels = CHANNELS;
-    desired.samples = BUFFER_SIZE;
-    desired.callback = audioCallback;
-    desired.userdata = this;
+    if (stream)
+        return true;
 
-    dev = SDL_OpenAudioDevice(nullptr, 0, &desired, &obtainedSpec, 0);
+    SDL_AudioSpec spec{};
+    spec.freq = SAMPLE_RATE;
+    spec.format = SDL_AUDIO_S16;
+    spec.channels = CHANNELS;
 
-    if (!dev)
+    stream = SDL_OpenAudioDeviceStream(SDL_AUDIO_DEVICE_DEFAULT_PLAYBACK, &spec, audioCallback, this);
+
+    if (!stream)
     {
         SDL_LogError(SDL_LOG_CATEGORY_AUDIO, "Couldn't open audio: %s", SDL_GetError());
         return false;
     }
 
-    // Start paused. EmulationSession will resume after SID has buffered samples.
-    SDL_PauseAudioDevice(dev, 1);
     return true;
 }
 
 void AudioOutput::pauseAudio()
 {
-    if (dev != 0)
-        SDL_PauseAudioDevice(dev, 1);
+    if (stream)
+    {
+        if (!SDL_PauseAudioStreamDevice(stream))
+            SDL_LogError(SDL_LOG_CATEGORY_AUDIO, "Couldn't pause audio: %s", SDL_GetError());
+    }
 }
 
 void AudioOutput::stopAudio()
 {
-    if (dev != 0)
+    if (stream)
     {
-        SDL_PauseAudioDevice(dev, 1);
-        SDL_CloseAudioDevice(dev);
-        dev = 0;
+        SDL_PauseAudioStreamDevice(stream);
+        SDL_DestroyAudioStream(stream);
+        stream = nullptr;
     }
 
     sid = nullptr;
@@ -92,6 +115,9 @@ void AudioOutput::stopAudio()
 
 void AudioOutput::resumeAudio()
 {
-    if (dev != 0)
-        SDL_PauseAudioDevice(dev, 0);
+    if (stream)
+    {
+        if (!SDL_ResumeAudioStreamDevice(stream))
+            SDL_LogError(SDL_LOG_CATEGORY_AUDIO, "Couldn't resume audio: %s", SDL_GetError());
+    }
 }
