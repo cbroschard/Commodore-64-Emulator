f2bb1a174b90593df73e78c9eea36cf9e661b1d3
Christopher Broschard <cbroschard@hotmail.com>
2026-08-07 23:03:54 -0400

    UserPortDevice: Skeleton class

---
+++ include/UserPort/UserPortDevice.h
@@ -0,0 +1,22 @@
+// Copyright (c) 2025 Christopher Broschard
+// All rights reserved.
+//
+// This source code is provided for personal, educational, and
+// non-commercial use only. Redistribution, modification, or use
+// of this code in whole or in part for any other purpose is
+// strictly prohibited without the prior written consent of the author.
+#ifndef USERPORTDEVICE_H
+#define USERPORTDEVICE_H
+
+class UserPortDevice
+{
+    public:
+        UserPortDevice();
+        virtual ~UserPortDevice();
+
+    protected:
+
+    private:
+};
+
+#endif // USERPORTDEVICE_H
