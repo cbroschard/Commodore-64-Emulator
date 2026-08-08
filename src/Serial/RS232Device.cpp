3b1d8ce716f203e692cb8fdc8f1281f36276eea1
Christopher Broschard <cbroschard@hotmail.com>
2026-08-07 22:42:23 -0400

    Moved RS232Device to Serial folders

--- src/Serial/RS232Device.cpp
+++ src/Serial/RS232Device.cpp
@@ -5,7 +5,7 @@
 // non-commercial use only. Redistribution, modification, or use
 // of this code in whole or in part for any other purpose is
 // strictly prohibited without the prior written consent of the author.
-#include "RS232Device.h"
+#include "Serial/RS232Device.h"
 
 RS232Device::RS232Device() :
     cycleAccumulator(0),
