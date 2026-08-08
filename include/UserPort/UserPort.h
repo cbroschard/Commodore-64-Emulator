e699071c84fac53dede5c62e75a84974488a6904
Christopher Broschard <cbroschard@hotmail.com>
2026-08-07 22:53:57 -0400

    UserPort skeleton classes

---
+++ include/UserPort/UserPort.h
@@ -0,0 +1,23 @@
+// Copyright (c) 2025 Christopher Broschard
+// All rights reserved.
+//
+// This source code is provided for personal, educational, and
+// non-commercial use only. Redistribution, modification, or use
+// of this code in whole or in part for any other purpose is
+// strictly prohibited without the prior written consent of the author.
+#ifndef USERPORT_H
+#define USERPORT_H
+
+
+class UserPort
+{
+    public:
+        UserPort();
+        virtual ~UserPort();
+
+    protected:
+
+    private:
+};
+
+#endif // USERPORT_H
---
+++ include/UserPort/UserPortRS232Adapter.h
@@ -0,0 +1,23 @@
+// Copyright (c) 2025 Christopher Broschard
+// All rights reserved.
+//
+// This source code is provided for personal, educational, and
+// non-commercial use only. Redistribution, modification, or use
+// of this code in whole or in part for any other purpose is
+// strictly prohibited without the prior written consent of the author.
+#ifndef USERPORTRS232ADAPTER_H
+#define USERPORTRS232ADAPTER_H
+
+
+class UserPortRS232Adapter
+{
+    public:
+        UserPortRS232Adapter();
+        virtual ~UserPortRS232Adapter();
+
+    protected:
+
+    private:
+};
+
+#endif // USERPORTRS232ADAPTER_H
