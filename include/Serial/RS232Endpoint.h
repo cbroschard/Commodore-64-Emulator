38124736fea8fc83f40d4d9fd8bda8045aa0faee
Christopher Broschard <cbroschard@hotmail.com>
2026-08-09 19:22:00 -0400

    RS232Endpoint: Initial release rs-232 endpoint abstraction

---
+++ include/Serial/RS232Endpoint.h
@@ -0,0 +1,24 @@
+// Copyright (c) 2025 Christopher Broschard
+// All rights reserved.
+//
+// This source code is provided for personal, educational, and
+// non-commercial use only. Redistribution, modification, or use
+// of this code in whole or in part for any other purpose is
+// strictly prohibited without the prior written consent of the author.
+#ifndef RS232ENDPOINT_H_INCLUDED
+#define RS232ENDPOINT_H_INCLUDED
+
+class RS232Endpoint
+{
+public:
+    virtual ~RS232Endpoint() = default;
+
+    virtual void reset() = 0;
+    virtual void tick() = 0;
+
+    virtual bool hasByte() const = 0;
+    virtual bool readByte(uint8_t& value) = 0;
+    virtual void writeByte(uint8_t value) = 0;
+};
+
+#endif // RS232ENDPOINT_H_INCLUDED
