40c073e99ed042569e5a62aeca678049de496001
Christopher Broschard <cbroschard@hotmail.com>
2026-09-05 14:36:35 -0400

    SID: Remove unused envelope timing tables

--- include/SID/SIDEnvelopeTables.h
+++ include/SID/SIDEnvelopeTables.h
@@ -29,24 +29,4 @@ static constexpr double SID_DECAY_RELEASE_S[16] =
     3.000, 9.000, 15.000, 24.000
 };
 
-// Per-envelope-step periods in NTSC SID cycles.
-// Envelope::setADSR() should scale these by:
-//     sidClockFrequency / SID_CLOCK_NTSC
-// so PAL keeps approximately the same real-time ADSR durations.
-static constexpr double SID_ATTACK_STEP_CYCLES_NTSC[16] =
-{
-    8.021, 32.086, 64.171, 96.257,
-    152.593, 224.599, 272.727, 320.856,
-    401.069, 1002.673, 2005.347, 3208.555,
-    4010.694, 12032.082, 20053.471, 32085.553
-};
-
-static constexpr double SID_DECAY_RELEASE_STEP_CYCLES_NTSC[16] =
-{
-    24.064, 96.257, 192.514, 288.770,
-    457.455, 673.561, 818.182, 962.567,
-    1203.208, 3008.020, 6016.041, 9625.694,
-    12032.082, 36096.247, 60160.412, 96256.659
-};
-
 #endif // SID_ENVELOPE_TABLES_H
