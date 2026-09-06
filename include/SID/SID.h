ca40c60c04f60479624db96a894ad062c66d39bb
Christopher Broschard <cbroschard@hotmail.com>
2026-09-06 15:32:28 -0400

    SID: Make envelope parameter helper static

--- include/SID/SID.h
+++ include/SID/SID.h
@@ -156,7 +156,7 @@ class SID
         // Helpers
         inline void refreshDataBusDecay() { sidBusDecayCycles = sidDataBusDecayCycles(sidModel_); }
         static uint16_t combineBytes(uint8_t high, uint8_t low);
-        void updateEnvelopeParameters(Voice& voice, const voiceRegisters& regs);
+        static void updateEnvelopeParameters(Voice& voice, const voiceRegisters& regs);
         void updateCutoffFromRegisters();
         void applyVoiceControl(Voice& voice, uint8_t oldControl, uint8_t newControl);
         void configureOscillatorSources();
