003229d30bcc6c063a4ca75128383b1ef8ae1cb0
Christopher Broschard <cbroschard@hotmail.com>
2026-09-10 21:55:37 -0400

    Vic: Pack PAL sprite fetches into half-cycle bus slots

--- include/Common/VideoMode.h
+++ include/Common/VideoMode.h
@@ -161,44 +161,44 @@ inline constexpr ModeConfig PAL_CONFIG =
 
     {
         {54, VicBusPhase::Phi1,
-         55, VicBusPhase::Phi2,
-         56, VicBusPhase::Phi1,
-         57, VicBusPhase::Phi2},
+         54, VicBusPhase::Phi2,
+         55, VicBusPhase::Phi1,
+         55, VicBusPhase::Phi2},
 
         {57, VicBusPhase::Phi1,
-         58, VicBusPhase::Phi2,
-         59, VicBusPhase::Phi1,
-         60, VicBusPhase::Phi2},
+         57, VicBusPhase::Phi2,
+         58, VicBusPhase::Phi1,
+         58, VicBusPhase::Phi2},
 
         {60, VicBusPhase::Phi1,
-         61, VicBusPhase::Phi2,
-         62, VicBusPhase::Phi1,
-          0, VicBusPhase::Phi2},
+         60, VicBusPhase::Phi2,
+         61, VicBusPhase::Phi1,
+         61, VicBusPhase::Phi2},
 
         { 0, VicBusPhase::Phi1,
-          1, VicBusPhase::Phi2,
-          2, VicBusPhase::Phi1,
-          3, VicBusPhase::Phi2},
+          0, VicBusPhase::Phi2,
+          1, VicBusPhase::Phi1,
+          1, VicBusPhase::Phi2},
 
         { 3, VicBusPhase::Phi1,
-          4, VicBusPhase::Phi2,
-          5, VicBusPhase::Phi1,
-          6, VicBusPhase::Phi2},
+          3, VicBusPhase::Phi2,
+          4, VicBusPhase::Phi1,
+          4, VicBusPhase::Phi2},
 
         { 6, VicBusPhase::Phi1,
-          7, VicBusPhase::Phi2,
-          8, VicBusPhase::Phi1,
-          9, VicBusPhase::Phi2},
+          6, VicBusPhase::Phi2,
+          7, VicBusPhase::Phi1,
+          7, VicBusPhase::Phi2},
 
         { 9, VicBusPhase::Phi1,
-         10, VicBusPhase::Phi2,
-         11, VicBusPhase::Phi1,
-         12, VicBusPhase::Phi2},
+          9, VicBusPhase::Phi2,
+         10, VicBusPhase::Phi1,
+         10, VicBusPhase::Phi2},
 
         {12, VicBusPhase::Phi1,
-         13, VicBusPhase::Phi2,
-         14, VicBusPhase::Phi1,
-         15, VicBusPhase::Phi2}
+         12, VicBusPhase::Phi2,
+         13, VicBusPhase::Phi1,
+         13, VicBusPhase::Phi2}
     }, // spriteFetchTiming
 
     SPRITE_STEAL_DATA0 | SPRITE_STEAL_DATA2
