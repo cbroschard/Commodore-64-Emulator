bba74f18cd6086364f757afe336341a581f56757
Christopher Broschard <cbroschard@hotmail.com>
2026-09-12 22:44:41 -0400

    AtomicPowerMapper: Add peek()

--- include/Cartridge/AtomicPowerMapper.h
+++ include/Cartridge/AtomicPowerMapper.h
@@ -45,11 +45,11 @@ class AtomicPowerMapper : public CartridgeMapper, public IHasButton
         uint8_t read(uint16_t address) override;
         void write(uint16_t address, uint8_t value) override;
 
+        uint8_t peek(uint16_t address) const override;
+
         bool loadIntoMemory(uint8_t bank) override;
 
         bool readDrivesBus(uint16_t address) const override;
-
-    protected:
 
     private:
         bool    freezeActive;
