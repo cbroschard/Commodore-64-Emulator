46cbc841f57a7c5aa427e85b7bda525d9af2f8aa
Christopher Broschard <cbroschard@hotmail.com>
2026-09-12 22:45:01 -0400

    KCSPowerMapper: Add peek()

--- include/Cartridge/KCSPowerMapper.h
+++ include/Cartridge/KCSPowerMapper.h
@@ -28,11 +28,11 @@ class KCSPowerMapper : public CartridgeMapper, public IHasButton
         uint8_t read(uint16_t address) override;
         void write(uint16_t address, uint8_t value) override;
 
+        uint8_t peek(uint16_t address) const override;
+
         bool loadIntoMemory(uint8_t bank) override;
 
         bool readDrivesBus(uint16_t address) const;
-
-    protected:
 
     private:
         bool applyMappingAfterLoad() override;
