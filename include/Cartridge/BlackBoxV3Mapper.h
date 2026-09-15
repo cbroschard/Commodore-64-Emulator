4612fe983eff26f52446c2c8d0287eabaea7ff19
Christopher Broschard <cbroschard@hotmail.com>
2026-09-14 21:42:36 -0400

    BlackBoxV3Mapper: Fix applyMappingAfterLoad() to be public

--- include/Cartridge/BlackBoxV3Mapper.h
+++ include/Cartridge/BlackBoxV3Mapper.h
@@ -27,10 +27,10 @@ class BlackBoxV3Mapper : public CartridgeMapper
 
         bool loadIntoMemory(uint8_t bank) override;
 
+        bool applyMappingAfterLoad() override;
+
     private:
         bool enabled;
-
-        bool applyMappingAfterLoad() override;
 
         void updateLines();
 };
